da5453d57b5500f91441190bee8f2c73b491d262
Hidekithiago <39059176+Hidekithiago@users.noreply.github.com>
2022-10-08 18:59:31 -0300

    modelo c#

---
+++ Modelo/modelo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace modelo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] email = {"a@hotmail.com", "b@hotmail.com"}; //Array
+            pessoa p1 = new pessoa(1, "HIDEKI", "26", email, 0);
+            Console.WriteLine(p1.Email[1]);
+            pessoa.addSaldo(p1, 25);
+            pessoa.addSaldo(p1, 50);
+            Thread.Sleep(10000);
+        }
+    }
+}
---
+++ Modelo/modelo/objeto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modelo
+{
+    internal class pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Idade { get; set; } = string.Empty;
+        public string[] Email { get; set; }
+        private float Saldo { get; set; } = 0;
+
+        public pessoa(int id, string nome, string idade, string[] email, float saldo)
+        {
+            Id = id;
+            Nome = nome;
+            Idade = idade;
+            Email = email;
+            Saldo = saldo;
+        }
+
+        public static float addSaldo(pessoa pessoa, float valor)
+        {
+            pessoa.Saldo = pessoa.Saldo + valor;
+            Console.WriteLine(pessoa.Saldo);
+            return pessoa.Saldo;
+        }
+    
+    }
+}
