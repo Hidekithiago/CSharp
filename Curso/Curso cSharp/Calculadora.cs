0a05a8f4c85f39435fafd4883399857611c5fa28
Hidekithiago <39059176+Hidekithiago@users.noreply.github.com>
2022-10-28 05:07:59 -0300

    Adicionando novas funcoes

---
+++ Curso/Curso cSharp/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web;
+
+public class Calculadora
+{
+    public double Valor1 { get;  set; }
+    public double Valor2 { get; set; }
+    public string Operador { get; set; }
+    public double Resultado { get; }
+
+    public Calculadora(double valor1, double valor2, string operador)
+    {
+        Valor1 = valor1;
+        Valor2 = valor2;
+        Operador = operador;
+    }
+
+    public static double RealizaCalculo(double valor1, double valor2, string operador)
+    {
+        double resultado = 0;
+        if (operador.Equals("+"))
+        {
+            resultado = valor1 + valor2;
+            Console.WriteLine($"Valor1 {valor1} \nValor2 {valor2}\n Resultado {resultado}");
+        }
+
+
+        else if (operador.Equals("-")) resultado = valor1 - valor2;
+        else if (operador.Equals("/")) resultado = valor1 / valor2;
+        else if (operador.Equals("*")) resultado = valor1 * valor2;
+        else if (operador.Equals("**"))
+        {
+            resultado = Math.Pow(valor1, valor2);
+        }
+        return resultado;
+    }
+    public class Execucao { }
+    /*
+    static void Main(string[] args)
+    {
+        Calculadora calculadora = new Calculadora(2, 3, "**");
+        
+        double resultado = RealizaCalculo(calculadora.Valor1, calculadora.Valor2, calculadora.Operador);
+        
+        
+        double a = Double.Parse(Console.ReadLine());
+        double b = Double.Parse(Console.ReadLine());
+        string c = Console.ReadLine();
+        calculadora = new Calculadora(a, b, c);
+        Console.WriteLine(calculadora);
+        resultado = RealizaCalculo(calculadora.Valor1, calculadora.Valor2, calculadora.Operador);
+    }*/
+}
+
---
+++ Curso/Curso cSharp/Class1.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_cSharp
+{
+    internal class Class1
+    {
+        private static string connectionString = "Server = 162.241.60.117; Uid = quaest71_treinamento; Pwd =treinamento123; database=quaest71_treinamento";
+        public static DataSet ConsultaQueryArmazena(string query)
+        {
+            using (var conexao = new MySqlConnection(connectionString))
+            {
+                DataSet ds = new DataSet();
+                MySqlCommand mycommand = new MySqlCommand(query, conexao);
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(mycommand))
+                {
+                    adapter.Fill(ds);
+                }
+                return ds;
+            }
+        }
+
+        public static void MostraLinhas(DataSet dtable)
+        {
+            foreach (DataTable tabela in dtable.Tables)
+            {
+                foreach (DataRow row in tabela.Rows)
+                {
+                    string linha = null;
+                    for (int i = 0; i < tabela.Columns.Count; i++) linha += row[i].ToString() + " ";
+                    Console.WriteLine(linha);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string query = "SELECT * FROM `medicoCRM` where uf = 'SP' and crm like '1%'";
+            var b = ConsultaQueryArmazena(query);
+        }
+
+    }
+}
