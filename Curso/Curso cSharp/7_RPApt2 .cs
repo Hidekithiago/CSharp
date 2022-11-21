4a2687c979f7f75462321e2005a13ab3fe4ef8f1
Hidekithiago <39059176+Hidekithiago@users.noreply.github.com>
2022-11-21 09:53:46 -0300

    Adicionando o Winium e programas

---
+++ Curso/Curso cSharp/7_RPApt2 .cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium; //Install Selenium.WebDriver
+using OpenQA.Selenium.Chrome;  //Selenium.WebDriver.ChromeDriver
+using System.Diagnostics;  //Usado para fechar o chrome
+
+using System.Threading;
+
+
+//Selenium.WebDriver.ChromeDriver
+
+public class _7_RPApt2
+{
+    private static IWebDriver driver;
+    private static void SeleniumGoogle()
+    {
+        driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+        driver.Navigate().GoToUrl(@"https://www.b3.com.br/pt_br/produtos-e-servicos/negociacao/renda-variavel/empresas-listadas.htm");
+        Thread.Sleep(1000);
+        driver.SwitchTo().Frame(0);
+        driver.FindElement(By.XPath("/html/body/app-root/app-companies-home/div/div/div/div/div[1]/div[2]/div/app-companies-home-filter-name/form/div/div[4]/button")).Click();
+        Thread.Sleep(1000);
+        driver.FindElement(By.XPath("/html/body/app-root/app-companies-search/div/form/div[1]/div/div/div[2]/nav/div/a[2]")).Click();
+
+        for (int i = 1; i < 10; i++)
+        {
+            Thread.Sleep(1000);
+            var razaoSocial = driver.FindElement(By.XPath($"/html/body/app-root/app-companies-search/div/form/div[2]/div[2]/table/tbody/tr[{i}]/td[1]/a")).Text;
+            var nomePregao = driver.FindElement(By.XPath($"/html/body/app-root/app-companies-search/div/form/div[2]/div[2]/table/tbody/tr[{i}]/td[2]/a")).Text;
+            var segmento = driver.FindElement(By.XPath($"/html/body/app-root/app-companies-search/div/form/div[2]/div[2]/table/tbody/tr[{i}]/td[3]/label[1]")).Text;
+            var codigo = driver.FindElement(By.XPath($"/html/body/app-root/app-companies-search/div/form/div[2]/div[2]/table/tbody/tr[{i}]/td[4]")).Text;
+
+            if (codigo.Equals("GRDR")){
+                driver.FindElement(By.XPath($"/html/body/app-root/app-companies-search/div/form/div[2]/div[2]/table/tbody/tr[{i}]/td[1]/a")).Click();
+                Thread.Sleep(1000);
+                var cnpj = driver.FindElement(By.XPath($"/html/body/app-root/app-companies-menu-select/div/app-companies-overview/div/div[1]/div/div/div[1]/p[2]")).Text;
+            }
+
+
+        }
+        
+    }
+    private static void FechaChrome()
+    {
+        Process[] chromeInstances = Process.GetProcessesByName("chrome");
+        foreach (Process p in chromeInstances) p.Kill();
+    }
+    
+    static void Main(string[] args)
+    {
+        SeleniumGoogle();
+    }
+    
+    
+}
---
+++ Curso/exemploWinium/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Automation;
+using System.Windows.Forms;
+using Winium.Cruciatus;
+using Winium.Cruciatus.Core;
+using Winium.Cruciatus.Extensions;
+
+namespace exemploWinium
+{
+    internal class Program
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+
+        static void Main(string[] args)
+        {
+
+            (int x, int y) = GetPosition();
+            MouseClick(x, y);
+        }
+
+        public void Calculadora()
+        {
+            var calc = new Winium.Cruciatus.Application("C:/windows/system32/calc.exe");
+            calc.Start();
+
+            var winFinder = By.Name("Calculadora").AndType(ControlType.Window);
+            var win = Winium.Cruciatus.CruciatusFactory.Root.FindElement(winFinder);
+
+            win.FindElementByUid("num6Button").Click();
+            win.FindElementByUid("plusButton").Click();
+            win.FindElementByUid("num4Button").Click();
+            win.FindElementByUid("equalButton").Click();
+        }
+
+        public void MoveMouseClick()
+        {
+
+        }
+
+        public static (int, int) GetPosition()
+        {
+            Thread.Sleep(5000);
+            return (Cursor.Position.X, Cursor.Position.Y);
+        }
+
+        public static void MouseClick(int x, int y)
+        {
+            SetCursorPos(x, y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+        }
+
+    }
+}
